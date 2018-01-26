1fd51d1585a1379e174fd5a219ed3939f6f3be21
karistol <kari.stolt@metropolia.fi>
2018-01-26 12:13:51 +0200

    f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListMediaComponent } from './list-media/list-media.component';
+import {MediaService} from './services/media.service';
+import {HttpClientModule} from '@angular/common/http';
+import {DigitransitService} from './services/digitransit.service';
+import { RoutesComponent } from './routes/routes.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListMediaComponent,
+    RoutesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, FormsModule
   ],
-  providers: [],
+  providers: [MediaService, DigitransitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
